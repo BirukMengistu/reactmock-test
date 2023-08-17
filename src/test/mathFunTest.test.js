dbd49fa5ed8457b9429d1c4254b86040be7ec852
BirukMengistu <sheger.shelf3@gmail>
2023-08-17 12:13:33 +0200

    test main branch

---
+++ src/test/mathFunTest.test.js
@@ -0,0 +1,17 @@
+import {Sum ,Power} from '../helper/mathFun'
+import {describe, expect, test} from '@jest/globals';
+
+//sum function Test
+describe('Math function',()=>{
+    test('sum function a + = c', ()=>{
+        expect(Sum(3,4)).toBe(7)
+        expect(Sum(4,4)).toBe(8)
+        expect(Sum(4.6,4)).toBe(8.)
+    })
+    
+    test('power function a = c', ()=>{
+        expect(Power(3)).toBe(9)
+    })
+
+
+})
