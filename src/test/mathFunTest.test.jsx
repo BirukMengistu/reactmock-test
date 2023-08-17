b201f2a0f30e66404c924da995f9ee855c960509
BirukMengistu <sheger.shelf3@gmail>
2023-08-17 12:55:53 +0200

    object function unit jest test

--- src/test/mathFunTest.test.jsx
+++ src/test/mathFunTest.test.jsx
@@ -12,6 +12,12 @@ describe('Math function',()=>{
     test('power function a = c', ()=>{
         expect(Power(3)).toBe(9)
     })
-
+    test('adding positive numbers is not zero', () => {
+        for (let a = 1; a < 10; a++) {
+          for (let b = 1; b < 10; b++) {
+            expect(a + b).not.toBe(0);
+          }
+        }
+      });
 
 })
